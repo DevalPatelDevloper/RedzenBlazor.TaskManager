4256fb08a24ce7d6ffdbdaff9a69a114086c94b5
Deval Patel <deval.p@simformsolutions.com>
2025-06-17 14:03:05 +0530

    Add project files.

---
+++ Redzen Blazor Vsix Template/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Redzen_Blazor_Vsix_Template.Models
+{
+        public enum TaskStatus { Pending, InProgress, Completed }
+
+        public class TaskItem
+        {
+            public Guid Id { get; set; } = Guid.NewGuid();
+            public string Title { get; set; } = string.Empty;
+            public string AssignedTo { get; set; } = string.Empty;
+            public TaskStatus Status { get; set; } = TaskStatus.Pending;
+        }
+}
